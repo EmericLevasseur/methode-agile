c3192c96dc7e46a22d6e8b00d688ce736286b346
Kévin Huang <Kvin Huang>
2016-12-06 12:58:17 +0100

    Initialisation

---
+++ header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	    <meta name="description" content="">
+	    <meta name="keywords" content="">
+	    <meta name="author" content="">
+	    <meta name="robots" content="">
+	    <title>Test</title>
+	    <link rel="icon" type="image/png" href="<?php bloginfo('url'); ?>/wp-content/themes/" />
+		<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
+	</head>
+<body>
