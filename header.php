d7a56734dbb2586ea33b8b0a0de9cdfaeb4e80c8
Alanlgf <alanlegoff78@gmail.com>
2016-12-07 10:42:27 +0100

    insertion script +  début navbar

--- header.php
+++ header.php
@@ -11,5 +11,34 @@
 	    <title>Test</title>
 	    <link rel="icon" type="image/png" href="<?php bloginfo('url'); ?>/wp-content/themes/" />
 		<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
+		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	</head>
 <body>
+
+<nav class="navbar navbar-fixed-top">
+            <div class="container-fluid">
+                <div class="navbar-header" id="navbarSmall">
+                    <a href="#home" data-target="home"><img class="nav-logo" src="/img/logo-min.png" alt="Betraying The Martyrs" /></a>
+                    <button aria-controls="navbar" aria-expanded="false" data-target="#navbar" data-toggle="collapse" class="navbar-toggle collapsed" type="button">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="navbar-collapse collapse" id="navbar">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#" data-target="news">News</a></li>
+                        <li><a href="#" data-target="tour">Tour</a></li>
+                        <li><a href="#" data-target="music">Music</a></li>
+                        <li><a href="#" data-target="biography">Biography</a></li>
+                        <li class="logo-min"><h1><a href="" data-target="home"><img class="nav-logo" src="<?php bloginfo('url'); ?>/wp-content/themes/methode-agile/img/logo-min.png" alt="Betraying The Martyrs" /></a></h1></li>
+                        <li><a href="#" data-target="video">Video</a></li>
+                        <li><a href="#" data-target="merch">Merch</a></li>
+                        <li><a href="" data-target="press">Press</a></li>
+                        <li><a href="" data-target="contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
