e9dba41a2aff84154000c3635abf5af777d1d551
Diridollou <diridolloumarwan@gmail.com>
2016-12-07 12:56:56 +0100

    bio
    bio

---
+++ bio.php
@@ -0,0 +1,40 @@
+
+<?php
+/**
+ * Template Name: BIO
+ */
+
+get_header();
+
+?>
+
+<!doctype html>
+
+	<body>
+	
+<section>
+	<div id="bio_son" class="container">
+		<div class="row">
+			<div class="col-md-6>">
+				<p>BETRAYING THE MARTYRS are based out of Paris, France and feature members from France and England, which adds a unique dynamic to their songwriting. Their stunning debut offering, Breathe In Life, saw them make a huge impact throughout North America and Europe touring relentlessly on such high profile tours as RockStar Mayhem Fest and AllStars, amongst a slew of other club treks. then followed Phantom, with their crazy cover of Let it Go. With this album BTM stepped up and unleashed whose intensity can't be matched. It is a venerable freight-train of aggression with blazing harmonies, captivating hooks and bombastic off-time precision that led them festivals like Graspop, With Full Force, Resurrection Fest and many more.
+				BTM are done working on their third record, and already revealed one massive track "THE GREAT DISILLUSION"
+				There are few bands within the scene that can match BTM's energy and intensity live and the new material is tailored made to their live performance. </p>
+			</div>
+
+			<div class="col-md-6>">
+				<p>
+					<?php 
+					echo do_shortcode("[soundcloud url=https://api.soundcloud.com/tracks/156661852/]");
+					?>
+				</p>
+			</div>
+
+		</div><!-- Fin de row -->
+
+	</div> <!-- Fin de container bio_son -->
+</section>
+
+
+
+
+	</body>
