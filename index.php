c8263aff42529afd703fcde34e004c708c817326
Alanlgf <alanlegoff78@gmail.com>
2016-12-07 11:32:01 +0100

    ajout navbar + smooth scrolling

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php
 	get_header();
 ?>
+<div style=" margin-top: 120px;" id="contact">
 <h1>Test</h1>
+
 <a href="<?php bloginfo('url'); ?>/wp-content/themes/methode-agile/register.php">Inscription</a>
 <a href="<?php bloginfo('url'); ?>/wp-content/themes/methode-agile/login.php">Connexion</a>
 <a href="<?php bloginfo('url'); ?>/wp-content/themes/methode-agile/contact.php">Contact</a>
+</div>
+
+
 <?php
 	get_footer();
 ?>
