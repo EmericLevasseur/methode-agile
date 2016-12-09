b51a9ed783f9c36f78e1d1c6e8243cf466dbd51b
Guyzzi <G-aristor@hotmail.fr>
2016-12-09 10:59:15 +0100

    test push guy

--- index.php
+++ index.php
@@ -2,15 +2,16 @@
 	get_header();
 
 ?>
+
 <div style=" margin-top: 120px;">
-<h1>Test</h1>
+<h1>test</h1>
 
 <a href="<?php bloginfo('url'); ?>/wp-content/themes/methode-agile/register.php">Inscription</a>
 <a href="<?php bloginfo('url'); ?>/wp-content/themes/methode-agile/login.php">Connexion</a>
 <a href="<?php bloginfo('url'); ?>/wp-content/themes/methode-agile/contact.php">Contact</a>
 <a href="<?php bloginfo('url'); ?>/wp-content/themes/methode-agile/bio.php">Bio/son</a>
 
-</div>
+</div> 
 
 <?php include("video.php"); ?>
 <section id="contact">
