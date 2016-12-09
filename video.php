1a76fbd4738788b05979937512b0db989fde1f37
Emeric Levasseur <levasseur.emeric@outlook.fr>
2016-12-09 11:26:15 +0100

    video

--- video.php
+++ video.php
@@ -1,9 +1,8 @@
-<?php  
-
-/**
-
-*Template Name: video
-
+<?php
+/*
+Template Name: Video
 */
-
+echo do_shortcode("[huge_it_videogallery id='1']");
 ?>
+
+<link rel="stylesheet" type="text/css" href="video.css">
