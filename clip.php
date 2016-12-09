b0de4bdf324e554a5205d087d0983cad625de3a8
Emeric Levasseur <levasseur.emeric@outlook.fr>
2016-12-09 12:02:32 +0100

    footer

--- clip.php
+++ clip.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+include("conf/db.php");
 /*
 Template Name: Video
 */
---
+++ footer.php
@@ -0,0 +1,35 @@
+<footer class="footer-distributed">
+
+	<div class="footer-right">
+
+		<a href="https://www.facebook.com/WeAreBetrayingTheMartyrs/?fref=ts"><i class="fa fa-facebook"></i></a>
+		<a href="https://twitter.com/martyrstweets"><i class="fa fa-twitter"></i></a>
+		<a href="https://www.instagram.com/baptistevigier/"><i class="fa fa-instagram"></i></a>
+		<a href="https://www.youtube.com/watch?v=D3cXF2lHAsQ"><i class="fa fa-youtube"></i></a>
+
+	</div>
+
+	<div class="footer-left">
+
+		<p class="footer-links">
+			<a href="#">News</a>
+			·
+			<a href="#">Bio</a>
+			·
+			<a href="#">Calendrier</a>
+			·
+			<a href="#">Clip</a>
+			·
+			<a href="#">Contact</a>
+			·
+			<a href="#">Espace Pro</a>
+		</p>
+
+		<p>BetrayingTheMartyrs &copy; 2016</p>
+	</div>
+
+</footer>
+
+</body>
+
+</html>
